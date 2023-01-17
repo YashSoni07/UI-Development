3311b17aeb8fb1e14f5e686373745dc935a963df
YashSoni07 <yashson647@gmail.com>
2023-01-17 09:08:48 +0530

    UI

---
+++ Examples/JavaScript/Eg1.js
@@ -0,0 +1,40 @@
+// Predind Date Types
+// String
+var a = "Hello..."
+console.log(a)
+console.log(typeof (a))
+
+// Number
+var b = 10
+console.log(b)
+console.log(typeof(b))
+
+// Boolean
+var c = true
+console.log(c)
+console.log(typeof(c))
+console.log(true*1)
+console.log(false*1)
+
+// Undefined
+var d 
+console.log(d)
+console.log(typeof(d))
+
+// What is diff btw Undefind nd null
+
+// Null
+var e = null
+console.log(e)
+console.log(typeof(e))
+
+// Arrays
+
+var f = [10,20,30,40,50] // in JavaSript
+console.log(f)
+console.log(typeof(f))
+
+// Object
+var g = {fistName: "Yash", listName:"Soni"}
+console.log(g)
+console.log(typeof(g))
---
+++ Examples/JavaScript/Eg2.js
@@ -0,0 +1,27 @@
+var job = "Admin"
+console.log(job)
+
+var job = "Developer"
+console.log(job)
+
+job = "QA"
+console.log(job)
+
+// You can not Add in let
+let myName = "Yash"
+console.log(myName)
+
+myName = "Shashwat"
+console.log(myName)
+
+
+// let myName = "Shashwat"
+// console.log(myName) // SyntaxError: Identifier 'myName' has already been declared
+
+// Const
+// You can not Add in const
+const  favouritMovie = "Avtar"
+console.log(favouritMovie)
+
+// favouritMovie = "Avtar2"
+// console.log(favouritMovie) // TypeError: Assignment to constant variable.
